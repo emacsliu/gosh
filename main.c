a3fa114b3c99287eeab0d02f8ac98a6744004dcc
Maurice Aiken <liuxulei@ghostcloud.cn>
2018-09-28 11:57:29 +0800

    update

--- main.c
+++ main.c
@@ -11,9 +11,61 @@ char* lsh_read_line();
 char** lsh_split_line(char* line);
 int lsh_execute(char** args);
 int lsh_launch(char** args);
-int ls_cd(char** args);
-int ls_help(char** args);
-int ls_exit(char** args);
+int lsh_cd(char** args);
+int lsh_help(char** args);
+int lsh_exit(char** args);
+int lsh_num_builtins();
+
+
+char* builtin_str[] = {
+	"cd",
+	"help",
+	"exit"
+};
+
+int (*builtin_func[]) (char**)  = {
+	&lsh_cd,
+	&lsh_help,
+	&lsh_exit
+};
+
+int lsh_num_builtins() {
+	return sizeof(builtin_str) / sizeof(char*);
+}
+
+int lsh_cd(char** args)
+{
+	if (args[1] == NULL) {
+		fprintf(stderr, "lsh: expected argument to cd\n");
+	} else {
+		if (chdir(args[1]) != 0) {
+			perror("lsh");
+		}
+	}
+
+	return 1;
+}
+
+int lsh_help(char** args)
+{
+	int i;
+	printf("LiuXulei's gosh:\n");
+	printf("Type program names and arguments, and hit enter\n");
+	printf("The following are build in\n");
+
+	for (i =0; i< lsh_num_builtins(); i++) {
+		printf(" %s\n", builtin_str[i]);
+	}
+
+	printf("Use the man command for information\n");
+
+	return 1;
+}
+
+int lsh_exit(char** args)
+{
+	return 0;
+}
 
 void lsh_loop()
 {
@@ -152,52 +204,3 @@ int lsh_launch(char** args)
 	return 1;
 }
 
-char* builtin_str[] = {
-	"cd",
-	"help",
-	"exit"
-};
-
-int (*builtin_func[]) (char**)  = {
-	&lsh_cd,
-	&lsh_help,
-	&lsh_exit
-};
-
-int lsh_num_builtins() {
-	return sizeof(builtin_str) / sizeof(char*);
-}
-
-int lsh_cd(char** args)
-{
-	if (args[1] == NULL) {
-		fprintf(stderr, "lsh: expected argument to cd\n");
-	} else {
-		if (chdir(args[1]) != 0) {
-			perror("lsh");
-		}
-	}
-
-	return 1;
-}
-
-int lsh_help(char** args)
-{
-	int i;
-	printf("LiuXulei's gosh:\n");
-	printf("Type program names and arguments, and hit enter\n");
-	printf("The following are build in\n");
-
-	for (i =0; i< lsh_num_builtins(); i++) {
-		printf(" %s\n", builtin_str[i]);
-	}
-
-	printf("Use the man command for information\n");
-
-	return 1;
-}
-
-int lsh_exit(char** args)
-{
-	return 0;
-}
